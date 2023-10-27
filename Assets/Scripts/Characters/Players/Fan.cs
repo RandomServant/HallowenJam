72ccfc6f70ea6f6964f9b270f23da99068ac947d
RandomServant <89282619+RandomServant@users.noreply.github.com>
2023-10-27 22:37:03 +1000

    Добавлен префаб вентилятора, Добавлен поворот головы за мышкой

--- Assets/Scripts/Characters/Players/Fan.cs
+++ Assets/Scripts/Characters/Players/Fan.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Fan : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Head;
+
+    private Rigidbody2D _rigidbody2D;
+    
     void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HeadRotate()
     {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         
+        if(!Head) return;
+        
+        Vector2 direction = mousePosition - Head.transform.position;
+        
+        float angle = Vector2.SignedAngle(Vector2.right, direction);
+        Head.transform.eulerAngles = new Vector3 (0, 0, angle);
     }
 }
