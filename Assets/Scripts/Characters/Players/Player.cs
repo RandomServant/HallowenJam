c19a8c28019e6e0f2a3701c3b9ad8a554bd0ca01
Pompon-Den <danil.ryzhkov.03@list.ru>
2023-10-28 13:27:39 +1000

    Добавил префаб окна смерти, и функцию рестарт уровня, функция смерти есть, но не активная

--- Assets/Scripts/Characters/Players/Player.cs
+++ Assets/Scripts/Characters/Players/Player.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    
+    public GameObject UIplayer;
+    public GameObject WindowDeath;
+
+    void Start()
+    {
+        Instantiate(UIplayer, transform.position, transform.rotation);
+    }
+
+    void Death()
+    {
+        UIplayer.SetActive(false);
+        Instantiate(WindowDeath, transform.position, transform.rotation);
+
+    }
+
 }
