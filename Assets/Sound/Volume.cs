387db6bbc6d1a047369b42a27fe73b1e3d077e9b
Pompon-Den <danil.ryzhkov.03@list.ru>
2023-10-30 04:08:04 +1000

    фикс слайдера и сохранения значения громкости

--- Assets/Sound/Volume.cs
+++ Assets/Sound/Volume.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Volume : MonoBehaviour
 {
     private AudioSource audioSrc;
+    [SerializeField] private Slider slider;
 
     void Start()
     {
         audioSrc = GetComponent<AudioSource>();
         audioSrc.volume = PlayerPrefs.GetFloat("Volume");
+        if (slider != null)
+        {
+            slider.value = audioSrc.volume;
+        }
+
     }
 
     void Update()
     {
         audioSrc.volume = PlayerPrefs.GetFloat("Volume");
+        //slider.value = audioSrc.volume;
     }
 
     public void SetVolume(float vol)
