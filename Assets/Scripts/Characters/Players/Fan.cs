51eb38acf3dedc0d79f02fc71e39047b5b95f1bc
RandomServant <89282619+RandomServant@users.noreply.github.com>
2023-10-29 12:27:14 +1000

    Добавлен поворот вентилятора в нужное направление по X

--- Assets/Scripts/Characters/Players/Fan.cs
+++ Assets/Scripts/Characters/Players/Fan.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Fan : Player
 {
-    public GameObject Head;
+    [SerializeField] private GameObject Head;
 
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
+    private SpriteRenderer _spriteRendererHead;
+    private SpriteRenderer _spriteRenderer;
 
     [Header("Settings")]
     [SerializeField] private float _maxDistanceInteraction = 5;
@@ -16,10 +18,12 @@ public class Fan : Player
 
     private int _currentLVL = 0;
 
-    void Start()
+    protected override void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _animator = GetComponentInChildren<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRendererHead = Head.GetComponent<SpriteRenderer>();
     }
     
     void Update()
@@ -42,19 +46,30 @@ public class Fan : Player
     private void HeadRotate()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
+
+        if (mousePosition.x >= transform.position.x + 0.1f)
+        {
+            _spriteRenderer.flipX = false;
+            _spriteRendererHead.flipX = false;
+        }
+        else if (mousePosition.x <= transform.position.x - 0.1f)
+        {
+            _spriteRendererHead.flipX = true;
+            _spriteRenderer.flipX = true;
+        }
+
         if(!Head) return;
         
         Vector2 direction = mousePosition - Head.transform.position;
         
-        float angle = Vector2.SignedAngle(Vector2.right, direction);
+        float angle = Vector2.SignedAngle(Vector2.right * (_spriteRenderer.flipX ? -1 : 1), direction);
         Head.transform.eulerAngles = new Vector3 (0, 0, angle);
     }
 
     private void PushMove()
     {
-        RaycastHit2D hit = Physics2D.Raycast(Head.transform.position, Head.transform.right, 
-            _maxDistanceInteraction, LayerMask.GetMask("Ground"));
+        RaycastHit2D hit = Physics2D.Raycast(Head.transform.position, Head.transform.right * 
+            (_spriteRenderer.flipX ? -1 : 1), _maxDistanceInteraction, LayerMask.GetMask("Ground"));
         
         _animator.SetFloat("Blend", ((float)_currentLVL + 1) / 3);
 
@@ -63,7 +78,8 @@ public class Fan : Player
             float distance = hit.distance;
             float force = (_maxDistanceInteraction - distance) * _pushForceforEveryLVL[_currentLVL];
             
-            _rigidbody2D.AddForce(-Head.transform.right * force, ForceMode2D.Impulse);
+            _rigidbody2D.AddForce(-Head.transform.right * (_spriteRenderer.flipX ? -1 : 1) * force, 
+                ForceMode2D.Impulse);
         }
     }
 }
